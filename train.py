70e0be3cf1c0b1de39bb0a0face2562d2b836e4a
mbertagna <78129017+mbertagna@users.noreply.github.com>
2025-02-21 00:35:26 -0600

    Update train.py

--- train.py
+++ train.py
@@ -3,7 +3,6 @@ import logging
 import os
 
 import torch
-from torch.nn import MSELoss
 from torch.optim import Adam
 
 from models.ResUNet import ResUNet
@@ -14,25 +13,31 @@ from utils.utils_data import get_dataloader
 from utils.utils_plot import plot_loss
 from utils.utils_train import MultiScaleLoss, ShapeConstraint, get_model_name
 
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 def train(model_name='Unrolled ADMM', n_iters=8, llh='Poisson', PnP=True, remove_SubNet=False, filter='Laplacian',
           n_epochs=10, lr=1e-4, loss='MultiScale',
-          data_path='/mnt/WD6TB/tianaoli/dataset/LSST_23.5_deconv/', train_val_split=0.8, batch_size=32,
+          data_path='/home/michaelbertagna/git/Galaxy-Deconv/simulated_datasets/LSST_23.5_deconv', train_val_split=0.8, batch_size=32,
           model_save_path='./saved_models/', pretrained_epochs=0):
     
     model_name = get_model_name(method=model_name, loss=loss, filter=filter, n_iters=n_iters, llh=llh, PnP=PnP, remove_SubNet=remove_SubNet)
     logger = logging.getLogger('Train')
     logger.info(' Start training %s on %s data for %s epochs.', model_name, data_path, n_epochs)
     
-    os.makedirs(model_save_path, exist_ok=True)
+    if not os.path.exists(model_save_path):
+        os.mkdir(model_save_path)
     
-    train_loader, val_loader = get_dataloader(data_path=data_path, train=True, train_val_split=train_val_split, batch_size=batch_size, num_workers=12)
+    train_loader, val_loader = get_dataloader(data_path=data_path, train=True, train_val_split=train_val_split, batch_size=batch_size, num_workers=0)
     
     if 'ADMM' in model_name:
         # model = Unrolled_ADMM(n_iters=n_iters, llh=llh, PnP=PnP, subnet=not remove_SubNet)
-        model = UnrolledADMMGaussian(n_iters=n_iters, PnP=PnP, subnet=True, analysis=False)
+        model = UnrolledADMMGaussian(n_iters=n_iters, PnP=PnP, subnet=True)
     elif 'Tikhonet' in model_name:
         model = Tikhonet(filter=filter)
     elif 'ShapeNet' in model_name:
@@ -40,7 +45,7 @@ def train(model_name='Unrolled ADMM', n_iters=8, llh='Poisson', PnP=True, remove
     elif model_name == 'ResUNet':
         model = ResUNet()
 
-    model.cuda()
+    model.to(device)
     if pretrained_epochs > 0:
         try:
             pretrained_file = os.path.join(model_save_path, f'{model_name}_{pretrained_epochs}epochs.pth')
@@ -52,9 +57,9 @@ def train(model_name='Unrolled ADMM', n_iters=8, llh='Poisson', PnP=True, remove
     if 'ShapeNet' in model_name or loss == 'Shape':
         loss_fn = ShapeConstraint(device=device, fov_pixels=48, n_shearlet=2, gamma=1)
     elif loss == 'MSE':
-        loss_fn = MSELoss().cuda()
+        loss_fn = torch.nn.MSELoss()
     elif loss == 'MultiScale':
-        loss_fn = MultiScaleLoss().cuda()
+        loss_fn = MultiScaleLoss()
     
     optimizer = Adam(params=model.parameters(), lr = lr)
 
@@ -65,7 +70,7 @@ def train(model_name='Unrolled ADMM', n_iters=8, llh='Poisson', PnP=True, remove
         train_loss = 0.0
         for idx, ((obs, psf, alpha), gt) in enumerate(train_loader):
             optimizer.zero_grad()
-            obs, psf, alpha, gt = obs.cuda(), psf.cuda(), alpha.cuda(), gt.cuda()
+            obs, psf, alpha, gt = obs.to(device), psf.to(device), alpha.to(device), gt.to(device)
             rec = model(obs, psf, alpha)
             loss = loss_fn(gt, rec)
 
@@ -79,21 +84,22 @@ def train(model_name='Unrolled ADMM', n_iters=8, llh='Poisson', PnP=True, remove
                 model.eval()
                 with torch.no_grad():
                     for _, ((obs, psf, alpha), gt) in enumerate(val_loader):
-                        obs, psf, alpha, gt = obs.cuda(), psf.cuda(), alpha.cuda(), gt.cuda()
+                        obs, psf, alpha, gt = obs.to(device), psf.to(device), alpha.to(device), gt.to(device)
                         rec = model(obs, psf, alpha)
                         loss = loss_fn(gt, rec)
                         val_loss += loss.item()
 
                 logger.info(" [{}: {}/{}]  train_loss={:.4g}  val_loss={:.4g}".format(
                                 epoch+1, idx+1, len(train_loader),
-                                train_loss, val_loss/len(val_loader)))
+                                train_loss,
+                                val_loss/len(val_loader)))
     
         # Evaluate on train & valid dataset after every epoch.
         train_loss = 0.0
         model.eval()
         with torch.no_grad():
             for _, ((obs, psf, alpha), gt) in enumerate(train_loader):
-                obs, psf, alpha, gt = obs.cuda(), psf.cuda(), alpha.cuda(), gt.cuda()
+                obs, psf, alpha, gt = obs.to(device), psf.to(device), alpha.to(device), gt.to(device)
                 rec = model(obs, psf, alpha)
                 loss = loss_fn(gt, rec)
                 train_loss += loss.item()
@@ -103,7 +109,7 @@ def train(model_name='Unrolled ADMM', n_iters=8, llh='Poisson', PnP=True, remove
         model.eval()
         with torch.no_grad():
             for _, ((obs, psf, alpha), gt) in enumerate(val_loader):
-                obs, psf, alpha, gt = obs.cuda(), psf.cuda(), alpha.cuda(), gt.cuda()
+                obs, psf, alpha, gt = obs.to(device), psf.to(device), alpha.to(device), gt.to(device)
                 rec = model(obs, psf, alpha)
                 loss = loss_fn(gt, rec)
                 val_loss += loss.item()
@@ -111,7 +117,8 @@ def train(model_name='Unrolled ADMM', n_iters=8, llh='Poisson', PnP=True, remove
 
         logger.info(" [{}: {}/{}]  train_loss={:.4g}  val_loss={:.4g}".format(
                         epoch+1, len(train_loader), len(train_loader),
-                        train_loss/len(train_loader), val_loss/len(val_loader)))
+                        train_loss/len(train_loader),
+                        val_loss/len(val_loader)))
         
         # Save model.
         if val_loss_min > val_loss or (epoch + 1) % 5 == 0:
@@ -126,11 +133,11 @@ def train(model_name='Unrolled ADMM', n_iters=8, llh='Poisson', PnP=True, remove
         plot_loss(train_loss_list, val_loss_list, epoch_min, model_save_path, model_name)
 
 
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
     parser = argparse.ArgumentParser(description='Arguments for training.')
-    parser.add_argument('--gpu', type=str, default='0', choices=['0', '1', '2'])
     parser.add_argument('--n_iters', type=int, default=8)
     parser.add_argument('--model', type=str, default='Unrolled_ADMM', choices=['Unrolled_ADMM', 'Tikhonet', 'ShapeNet', 'ResUNet'])
     parser.add_argument('--llh', type=str, default='Gaussian', choices=['Gaussian', 'Poisson'])
@@ -142,12 +149,10 @@ if __name__ == "__main__":
     parser.add_argument('--train_val_split', type=float, default=0.9)
     parser.add_argument('--batch_size', type=int, default=32)
     parser.add_argument('--pretrained_epochs', type=int, default=0)
-    args = parser.parse_args()
+    opt = parser.parse_args()
 
-    # Select GPU.
-    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
 
-    train(model_name=args.model, n_iters=args.n_iters, llh=args.llh, PnP=True, remove_SubNet=args.remove_SubNet, filter=args.filter,
-          n_epochs=args.n_epochs, lr=args.lr, loss=args.loss,
-          data_path='/mnt/WD6TB/tianaoli/dataset/LSST_23.5_deconv/', train_val_split=args.train_val_split, batch_size=args.batch_size,
-          model_save_path='./saved_models_test/', pretrained_epochs=args.pretrained_epochs)
+    train(model_name=opt.model, n_iters=opt.n_iters, llh=opt.llh, PnP=True, remove_SubNet=opt.remove_SubNet, filter=opt.filter,
+          n_epochs=opt.n_epochs, lr=opt.lr, loss=opt.loss,
+          data_path='/home/michaelbertagna/git/Galaxy-Deconv/simulated_datasets/LSST_23.5_deconv', train_val_split=opt.train_val_split, batch_size=opt.batch_size,
+          model_save_path='./saved_models_2024/', pretrained_epochs=opt.pretrained_epochs)
