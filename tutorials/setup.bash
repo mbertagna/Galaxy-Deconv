d3d0e894646ef90b183c7a626570eaa16bbc0cd0
mbertagna <78129017+mbertagna@users.noreply.github.com>
2025-03-17 18:15:46 -0500

    remove extra files

--- tutorials/setup.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-python3.11 -m venv Galaxy-Deconv.env
-source Galaxy-Deconv.env/bin/activate
-pip install -r requirements.txt
-pip install opencv-python-headless pandas
-galsim_download_cosmos -s 23.5
